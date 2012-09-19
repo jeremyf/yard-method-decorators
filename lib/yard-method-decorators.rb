b95558113411e956d5a5dbd7036e69f83676fe2d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-19 10:12:52 -0400

    Initial project commit

---
+++ lib/yard-method-decorators.rb
@@ -0,0 +1,9 @@
+require "yard-method-decorators/version"
+
+module Yard
+  module Method
+    module Decorators
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/yard-method-decorators/version.rb
@@ -0,0 +1,7 @@
+module Yard
+  module Method
+    module Decorators
+      VERSION = "0.0.1"
+    end
+  end
+end
